3d6fa9ef19c87eaa0fd9d92d0a7be9a91a442b9c
lmduong1001-web <lmduong1001@gmail.com>
2026-03-20 09:32:44 +0700

    network

--- server.bat
+++ server.bat
@@ -1,9 +1,17 @@
 @echo off
 cd /d %~dp0
 
+echo ===== ADD =====
 git add .
+
+echo ===== COMMIT =====
 git commit -m "network"
+
+echo ===== PULL (rebase) =====
+git pull origin master --rebase
+
+echo ===== PUSH =====
 git push origin master
 
-echo DONE!
+echo ===== DONE =====
 pause
